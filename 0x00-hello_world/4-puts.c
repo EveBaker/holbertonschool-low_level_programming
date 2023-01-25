ad289b8f351cfc1206464d1a60ce889dfdf2d37b
Evebaker <Evebaker0162@gmail.com>
2023-01-25 09:28:51 -0800

    C programming

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,7 @@
 /**
  * main - prints "Programming is like building a multilingual puzzle 
  * Return:Always 0.
-*/
-
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
