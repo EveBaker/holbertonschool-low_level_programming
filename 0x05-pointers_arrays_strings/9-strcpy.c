2b94b8019090b2cb5ca60f3db867025062c2836a
Evebaker <Evebaker0162@gmail.com>
2023-02-06 14:48:57 -0800

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,8 +7,10 @@
 * Return: the pointer to dest
 */
 
-char *_strcpy(char *dest, char *src){
+char *_strcpy(char *dest, char *src)
+{
 	int i;
+	
 	for (i = 0; src[i] != '\0'; i++)
 	{
 	dest[i] = src[i];
