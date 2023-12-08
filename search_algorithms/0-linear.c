9b3f4a269bd1d32fafcc9384409d4ba68f6c5c97
Eve Baker <103212241+EveBaker@users.noreply.github.com>
2023-12-08 19:04:28 +0000

    work in prog

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,19 @@
+#include "search.h"
+
+/*
+ * linear_search - Searches for a value in an array of integers
+ *                 using the Linear search algorithm.
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+    size_t 1;
+
+    if (array == NULL)
+    return -1;
+
+    for (i = 0; 1 < size; i++)
+    {
+        printf("Value checked array[%zu] = [%d]\n", i, array[i]);
+    }
+}
